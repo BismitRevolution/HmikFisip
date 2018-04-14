63a0ddcddfa1eaf6f21da667907f7dbba3275011
Nabila Fathia <nabilafathiaz@gmail.com>
2018-04-14 17:15:48 +0700

    socmed bar done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -158,7 +158,15 @@ HMIK UI 2017 memiliki harapan terhadap lingkungan Ilmu Komunikasi UI yang dapat 
 
 
 <div class="kontak-kami">
+    <!--
+    <div class="video-section">
+        <video autoplay muted loop id="videoHMIK" poster="videoGIF.gif">
+            <source src="{{ asset('video/Video Profile HMIK UI 2017.mp4')}}" type="video/mp4">
+        </video>
+    </div>
+-->
     <div class="container">
+        <hr/>
         <div class="icons row">
             <a class="col-3 text-center" href="https://twitter.com/hmikui" target="_blank">
                 <i class="fab fa-twitter"></i>
