d9a8bab77b14a54eca4ecdaf6eeb60c64758e81b
dhafin <dhafins18@gmail.com>
2018-05-21 00:22:39 +0700

    navbar, header, tentang kami

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,17 +1,20 @@
 <nav class="navbar navbar-expand-lg fixed-top navbar-light new-navbar">
-    <div class="container-fluid">
-
-        <a class="navbar-brand js-scroll-trigger" href="#home">
-            <img src="{{ asset('img/img-logo.png') }}" alt="Logo HMIK FISIP UI" class="navbar-logo" id="nav-logo">
+    <div class="navbar-brand">
+        <div class="background-logo" id="logo"></div>
+        <a class="js-scroll-trigger" href="#home">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo HMIK" class="navbar-logo" id="nav-logo">
+            <img src="{{ asset('img/logo-word.png') }}" alt="HMIK FISIP UI" class="navbar-logo" id="nav-logo-word">
         </a>
+    </div>
 
+    <div class="container-fluid navbar-container">
         <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar-main" aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbar-main">
             <ul class="nav navbar-nav text-uppercase ml-auto text-center">
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" data-toggle="collapse" data-target=".navbar-collapse.show" href="#header">HOME</a></li>
+                <li class="nav-item home-link"><a class="nav-link js-scroll-trigger" data-toggle="collapse" data-target=".navbar-collapse.show" href="#header">HOME</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" data-toggle="collapse" data-target=".navbar-collapse.show" href="#tentang-kami">TENTANG KAMI</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" data-toggle="collapse" data-target=".navbar-collapse.show" href="#profil">PROFIL</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" data-toggle="collapse" data-target=".navbar-collapse.show" href="#pojok-mahasiswa">POJOK MAHASISWA</a></li>
